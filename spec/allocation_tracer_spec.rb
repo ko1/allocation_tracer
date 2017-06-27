3c1688c93efb85f3fcd20e1eafef2ac8c9d3c3a8
Akira Matsuda <ronnie@dio.jp>
2017-06-27 19:04:45 +0900

    :warning: assigned but unused variable - a, b

--- spec/allocation_tracer_spec.rb
+++ spec/allocation_tracer_spec.rb
@@ -139,8 +139,8 @@ describe ObjectSpace::AllocationTracer do
         line = __LINE__ + 3
         ObjectSpace::AllocationTracer.setup(%i(path line type))
         result = ObjectSpace::AllocationTracer.trace do
-          a = [Object.new]
-          b = {Object.new => 'foo'}
+          _a = [Object.new]
+          _b = {Object.new => 'foo'}
         end
 
         expect(result.length).to be 5
@@ -155,8 +155,8 @@ describe ObjectSpace::AllocationTracer do
         line = __LINE__ + 3
         ObjectSpace::AllocationTracer.setup(%i(path line class))
         result = ObjectSpace::AllocationTracer.trace do
-          a = [Object.new]
-          b = {Object.new => 'foo'}
+          _a = [Object.new]
+          _b = {Object.new => 'foo'}
         end
 
         expect(result.length).to be 5
